2fae2d794ee0cbcab1d2dc5db0b1e146b4077bd4
mahde <m@p.c>
2026-02-08 22:04:48 -0500

    ui changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,28 +6,25 @@ const Navbar = () => {
     return (
         <AppBar position="static" sx={{ backgroundColor: '#0D1E20' }}>
             <Toolbar sx={{ justifyContent: 'space-between' }}>
-                <Button 
-                    component={Link} 
-                    to="/"
+                <Typography 
+                    variant="h6"
                     sx={{ 
-                        fontSize: '2rem',
+                        fontSize: '1.6rem',
                         left: '10%',
+                        marginLeft: '5%',
+                        marginRight: '5%',
                         fontWeight: 'bold',
-                        textTransform: 'none',
-                        color: '#F5C06A',
-                        '&:hover': { 
-                            backgroundColor: 'rgba(245, 192, 106, 0.1)' 
-                        }
+                        color: '#F5C06A'
                     }}
                 >
-                    BSS Wiki
-                </Button>
+                    Border and Surveillance Syndicate
+                </Typography>
                 <Box sx={{ mr: '15%' }}>
                     <Button 
                         component={Link} 
                         to="/"
                         sx={{ 
-                            fontSize: '1.75rem',
+                            fontSize: '1.4rem',
                             py: 2,
                             px: 4,
                             color: '#F5C06A',
@@ -42,7 +39,7 @@ const Navbar = () => {
                         component={Link} 
                         to="/wiki"
                         sx={{ 
-                            fontSize: '1.75rem',
+                            fontSize: '1.4rem',
                             py: 2,
                             px: 4,
                             color: '#F5C06A',
@@ -53,21 +50,6 @@ const Navbar = () => {
                     >
                         Wiki
                     </Button>
-                    {/* <Button 
-                        component={Link} 
-                        to="/map"
-                        sx={{ 
-                            fontSize: '1.3rem',
-                            py: 2,
-                            px: 4,
-                            color: '#F5C06A',
-                            '&:hover': { 
-                                backgroundColor: 'rgba(245, 192, 106, 0.1)' 
-                            }
-                        }}
-                    >
-                        Map
-                    </Button> */}
                 </Box>
             </Toolbar>
         </AppBar>
